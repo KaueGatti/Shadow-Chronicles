97a9b59fbc525c5f464eabfbbdc675f4eacf201b
KaueGatti <kauegattti@gmail.com>
2025-02-03 15:06:24 -0300

    Adicionando página de status e script jogo.js

--- jogo/jogo.js
+++ jogo/jogo.js
@@ -1,58 +1,51 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const name = localStorage.getItem('name');
-    const gender = localStorage.getItem('gender');
-    const classe = localStorage.getItem('class');
-    
-        if (gender) {
-        const genderText = document.querySelector('.gender');
-        if (genderText) {
-            genderText.textContent = gender === 'female' ? 'Feminino' : 'Masculino';
-        }
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  const name = localStorage.getItem("name");
+  const gender = localStorage.getItem("gender");
+  const classe = localStorage.getItem("class");
 
-    if (classe) {
-        const classeText = document.querySelector('.class');
-        if (classeText) {
-            classeText.textContent = classe === 'warrior' ? 'Guerreiro' : 'Mago';
-        }
-        const mp = document.querySelector('.mp');
-        if (classe === 'warrior') {
-            mp.style.display = 'none'
-        }
+  if (gender) {
+    const genderText = document.querySelector(".gender");
+    if (genderText) {
+      genderText.textContent = gender === "female" ? "Feminino" : "Masculino";
     }
-    
-    if (name) {
-        document.querySelectorAll('.name').forEach((element) => {
-            element.textContent = name;
-        });
+  }
+
+  if (classe) {
+    if (classe === "warrior") {
+      const containerMana = document.querySelector(".containerMana");
+      containerMana.style.display = "none";
     }
+  }
+
+  if (name) {
+    document.body.innerHTML = document.body.innerHTML.replace(/\$name/g, name);
+  }
 });
-
 
 // buttons
 
-const btnStatus = document.querySelector('#status');
-const btnHab = document.querySelector('#hab');
-const btnQuests = document.querySelector('#quests');
-const btnShop = document.querySelector('#shop');
-const btnBag = document.querySelector('#bag');
+const btnStatus = document.querySelector("#status");
+const btnHab = document.querySelector("#hab");
+const btnQuests = document.querySelector("#quests");
+const btnShop = document.querySelector("#shop");
+const btnBag = document.querySelector("#bag");
 
-btnStatus.addEventListener('click', function() {
-    window.location.href = 'extras/status.html';
+btnStatus.addEventListener("click", function () {
+  window.location.href = "extras/status.html";
 });
 
-btnHab.addEventListener('click', function() {
-    window.location.href = 'extras/habilidades.html';
+btnHab.addEventListener("click", function () {
+  window.location.href = "extras/habilidades.html";
 });
 
-btnQuests.addEventListener('click', function() {
-    window.location.href = 'extras/quests.html';
+btnQuests.addEventListener("click", function () {
+  window.location.href = "extras/quests.html";
 });
 
-btnShop.addEventListener('click', function() {
-    window.location.href = 'extras/loja.html';
+btnShop.addEventListener("click", function () {
+  window.location.href = "extras/loja.html";
 });
 
-btnBag.addEventListener('click', function() {
-    window.location.href = 'extras/inventario.html';
+btnBag.addEventListener("click", function () {
+  window.location.href = "extras/inventario.html";
 });
